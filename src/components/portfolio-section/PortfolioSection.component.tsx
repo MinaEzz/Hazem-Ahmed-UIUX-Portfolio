6859dfef03c8d74717347a490a41e65b9fdf0cf3
mina ezzat <minaezzat98@gmail.com>
2025-12-07 18:54:44 +0200

    feat(supabase): integrate Supabase and display projects
    - Added Supabase client configuration
    - Created  table in Supabase and inserted initial records
    - Fetched projects data from Supabase
    - Implemented desktop UI to render projects list
    - Added loading state while fetching
    - Mobile UI is pending

--- src/components/portfolio-section/PortfolioSection.component.tsx
+++ src/components/portfolio-section/PortfolioSection.component.tsx
@@ -5,9 +5,13 @@ import SectionHeader from "../common/section-header/SectionHeader.component";
 import DesktopPortfolio from "./desktop-portfolio/DesktopPortfolio.component";
 import MobilePortfolio from "./mobile-portfolio/MobilePortfolio.component";
 import FilterButtons from "./filter-buttons/FilterButtons.component";
+import { useGetProjects } from "@/hooks/useGetProjects.hook";
+import { RingLoader } from "react-spinners";
 
 export default function PortfolioSection() {
   const screenSize = useScreenSize();
+  const { projects, isLoading } = useGetProjects();
+
   return (
     <section
       id="projects"
@@ -19,7 +23,18 @@ export default function PortfolioSection() {
           <SectionHeader id="portfolio-heading" title="Portfolio">
             <FilterButtons />
           </SectionHeader>
-          {screenSize > 850 ? <DesktopPortfolio /> : <MobilePortfolio />}
+          {isLoading ? (
+            <div className="w-full flex flex-col items-center text-center gap-2">
+              <RingLoader size={102} color="#2231ff" className="mx-auto" />
+              <span className="text-xl text-[#C4C2C2] capitalize">
+                Loading...
+              </span>
+            </div>
+          ) : screenSize > 850 ? (
+            <DesktopPortfolio projects={projects} />
+          ) : (
+            <MobilePortfolio />
+          )}
         </SectionContentWrapper>
       </div>
     </section>
--- src/components/portfolio-section/desktop-portfolio/project-card/ProjectCard.types.ts
+++ src/components/portfolio-section/desktop-portfolio/project-card/ProjectCard.types.ts
@@ -3,5 +3,5 @@ import { StaticImport } from "next/dist/shared/lib/get-img-props";
 export default interface IProjectCardProps {
   image: string | StaticImport;
   title: string;
-  href: string;
+  // href: string;
 }
